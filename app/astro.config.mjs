076cbde60a7e4ba0ad8916c81b72290eca6104cf
Thai Nguyen <nnthai94@gmail.com>
2025-09-29 20:18:16 +0700

    Add robots.txt

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,20 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://thao-portfolio.com",
   vite: {
     plugins: [tailwindcss()]
   },
 
   server: {
-    watch: {
-      usePolling: true,
-    },
   },
 
-  integrations: [icon()]
+  integrations: [icon(), sitemap(), robotsTxt()]
 });
